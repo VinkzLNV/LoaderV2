be03dc5cc947a98743331ec7c0fb99273f350858
VinkzLNV <150276103+VinkzLNV@users.noreply.github.com>
2023-11-08 16:56:16 -0500

    Update script test.lua

--- script test.lua
+++ script test.lua
@@ -47,52 +47,7 @@ SSection:CreateSlider({ -- IMPORTANT: This function does not return anything, pl
 MSection:CreateButton({
     Name = "AutoParry"; -- required: name of element
     Callback = function() -- required: function to be called when button is pressed
-        script.Parent.Frame.ToggleFrame.Circle:TweenPosition(UDim2.new(0.2,0,0.5,0),Enum.EasingDirection.InOut,Enum.EasingStyle.Linear,0.09)
-			script.Parent.Frame.ToggleFrame.BackgroundColor3 = Color3.fromRGB(22, 21, 22)
-			script.Parent.Parent.Text.Value = script.Parent.Parent.Text.Value.. "" ..script.Parent.Name 
-			onoff = false
-		else
-			wait(0.2)
-			onoff = true
-			script.Parent.Parent.Text.Value = script.Parent.Parent.Text.Value.. "" ..script.Parent.Name 
-			script.Parent.Frame.ToggleFrame.BackgroundColor3 = Color3.fromRGB(52, 49, 52)
-			script.Parent.Frame.ToggleFrame.Circle:TweenPosition(UDim2.new(0.8,0,0.5,0),Enum.EasingDirection.InOut,Enum.EasingStyle.Linear,0.09)
-		end
-	end)
-end;
-task.spawn(C_81);
--- StarterGui.MainScreenGui.Frame.MainFrame.CombatFrame.UI.MainPage.viewpart.LocalScript
-local function C_88()
-	local script = G2L["88"];
-	local player = game.Players.LocalPlayer
-	local character = player.Character
-	local visualizer = Instance.new("Part")
-	visualizer.Material = "ForceField"
-	visualizer.Color = Color3.fromRGB(146, 255, 153)
-	visualizer.Transparency = 0.5
-	visualizer.Anchored = false
-	visualizer.CanCollide = false
-	visualizer.CastShadow = false
-	visualizer.Size = Vector3.new(13.46, 13.46, 13.46)
-	visualizer.Shape = "Ball"
-	function givePet (player)
-		if player then
-			local humRootPart = character.HumanoidRootPart
-			local newPet = visualizer:Clone()
-			newPet.Parent = character
-
-			local bodyPos = Instance.new("BodyPosition", newPet)
-			bodyPos.MaxForce = Vector3.new(math.huge, math.huge, math.huge)
-			bodyPos.D = "200"
-			while wait() do
-				bodyPos.Position = humRootPart.Position
-			end
-		end
-	end
-	script.Parent.MouseButton1Click:Connect(function()
-		givePet(player)
-	end)
-end;
+ print("Dont Work Rn ")
     end
 })
 
