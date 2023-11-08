750ef7de25faccd726263e03762f63ddc76d9a2f
VinkzLNV <150276103+VinkzLNV@users.noreply.github.com>
2023-11-08 17:07:29 -0500

    Update script test.lua

--- script test.lua
+++ script test.lua
@@ -1,54 +1,41 @@
-local Atlas = loadstring(game:HttpGet("https://siegehub.net/Atlas.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/forumsLib/main/source.lua"))()
+local Forums = Library.new("GH Galexy Hub")
 
-local UI = Atlas.new({
-    Name = "GH GamingHub"; -- script name (required)
-    ConfigFolder = "MyScript"; -- folder name to save configs to, set this to nil if you want to disable configs saving
-    Credit = "Made By Zinkz And Vinkz"; -- text to appear if player presses the "i" button on their UI (optional)
-    Color = Color3.fromRGB(255,0,0); -- theme color for UI (required)
-    Bind = "LeftControl"; -- keybind for toggling the UI, must be a valid KeyCode name
-    -- Atlas Loader:
-    UseLoader = true; -- whether to use Atlas Loader or not, if set to false than CheckKey and Discord are ignored
-    FullName = "My Script Hub"; -- works if UseLoader is set to true, can be same as Name argument, shown on loader frame
-    CheckKey = function(inputtedKey) -- works if UseLoader is set to true, where inputtedKey is the key inputted by the player
-        return inputtedKey=="YesIUseFree"
-    end;
-    Discord = "https://discord.gg/xu5dDS3Pb9"; -- works if UseLoader is set to true, will be copied to clipboard if player presses "Copy Invite" button
-})
+local preview = Forums:NewSection("Main")
+local preview1 = Forums:NewSection("Extra")
 
-local MyPage = UI:CreatePage("Main")
+local btn = preview:NewButton("This Button Will Update", function()
+    print("Button")
+end)
 
-local MSection = MyPage:CreateSection("Main Stuff")
+preview:Seperator()
 
-})
+preview1:NewSlider("WalkSpeed", 0, 400, function(value)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+end)
 
-UI:Notify({
-  Title = "Welcome To GamingHub";
-  Content = "Fr.";
-})
+preview1:Seperator()
 
-local SSection = SPage:CreateSection("My Section")
-
-SSection:CreateSlider({ -- IMPORTANT: This function does not return anything, please modify flags directly in order to read or update toggle values. SCROLL TO BOTTOM OF PAGE TO SEE HOW TO MODIFY FLAGS
-    Name = "Speed"; -- required: name of element
-    Flag = "Walkspeed"; -- required: unique flag name to use
-    Min = 0; -- required: slider minimum drag
-    Max = 100; -- required: slider maximum drag (Max>Min or else script will error)
-    AllowOutOfRange = true; -- optional: determines whether the player can enter values outside of range Min:Max when typing in the TextBox. If left nil, this is false
-    Digits = 2; -- optional: digits for rounding when dragging or entering values, default is 0 (whole numbers)
-    Default = 15; -- optional: default value for slider, will be used if config saving is disabled and there is no saved data, will be the Min value if left nil
-    Callback = function(newValue) -- optional: function that will be called whenever slider flag is changed
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = currentValue 
-    end;
-    -- Scroll to the bottom of the page to read more about the following two:
-    Warning = "Hi ?"; -- optional: this argument is used in all elements (except for Body) and will indicate text that will appear when the player hovers over the warning icon
-    WarningIcon = 12345; -- optional: ImageAssetId for warning icon, will only be used if Warning is not nil, default is yellow warning icon.
-})
-
-MSection:CreateButton({
-    Name = "AutoParry"; -- required: name of element
-    Callback = function() -- required: function to be called when button is pressed
- print("Dont Work Rn ")
+getgenv().stt = false
+local toggle = preview1:NewToggle("Status: ", function(status)
+    getgenv().stt = status
+end)
+coroutine.wrap(function()
+    while wait() do
+        toggle:Update("Status: "..tostring(getgenv().stt))
     end
-})
+end)()
 
+preview1:Seperator()
 
+local plrTable = {}
+preview1:NewButton("Update Player List", function()
+    for i,v in pairs(game.Players:GetChildren()) do
+        if v ~= game.Players.LocalPlayer then
+            table.insert(plrTable, v.Name)
+        end
+    end
+    drop:Refresh(plrTable)
+end)
+
+preview1:Seperator()
